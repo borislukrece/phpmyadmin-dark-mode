2245e117b1b3258c3da138931114965b88d3f334
Chelodev M.B <127858423+borislukrece@users.noreply.github.com>
2024-10-02 15:48:40 +0000

    Added background to toggle button

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,29 +4,43 @@ document.addEventListener("DOMContentLoaded", () => {
   if (toggleButton) {
     chrome.storage.local.get("darkMode", ({ darkMode }) => {
       toggleButton.textContent = darkMode
-        ? "Disable Dark Mode"
-        : "Enable Dark Mode";
+        ? "Toggle Dark Mode"
+        : "Toggle Dark Mode";
     });
 
     toggleButton.addEventListener("click", function () {
+      const btn = this;
+
       chrome.storage.local.get("darkMode", function (data) {
         const isDarkMode = !data.darkMode;
 
         chrome.storage.local.set({ darkMode: isDarkMode }, function () {
           toggleButton.textContent = isDarkMode
-            ? "Disable Dark Mode"
-            : "Enable Dark Mode";
+            ? "Toggle Dark Mode"
+            : "Toggle Dark Mode";
 
           chrome.tabs.query({}, function (tabs) {
             tabs.forEach((tab) => {
               if (
                 tab.url &&
                 (tab.url.includes("/phpmyadmin/") ||
-                  tab.url.includes("auth-db"))
+                  tab.url.includes("hstgr.io"))
               ) {
                 chrome.tabs.sendMessage(tab.id, { darkMode: isDarkMode });
               }
             });
+
+            btn.animate(
+              [
+                { backgroundColor: "#509F3AFF" },
+                { color: "#2AFA00FF" },
+                { borderColor: "#3E8923FF" },
+              ],
+              {
+                duration: 500,
+                iterations: 1,
+              }
+            );
           });
         });
       });
